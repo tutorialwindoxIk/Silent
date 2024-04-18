539a80a733b296fd421cd9fc066826131c4752c1
ZXCVBNMASDFGHJKLQWERTYUIOP0786 <165553468+ZXCVBNMASDFGHJKLQWERTYUIOP0786@users.noreply.github.com>
2024-04-18 15:38:36 +0530

    Add files via upload

--- LUCKYMUSIC/__init__.py
+++ LUCKYMUSIC/__init__.py
@@ -1,1 +1,28 @@
+from LUCKYMUSIC.core.bot import RAJA
+from LUCKYMUSIC.core.dir import dirr
+from LUCKYMUSIC.core.git import git
+from LUCKYMUSIC.core.userbot import Userbot
+from LUCKYMUSIC.misc import dbb, heroku
 
+from SafoneAPI import SafoneAPI
+from .logging import LOGGER
+
+dirr()
+git()
+dbb()
+heroku()
+
+app = RAJA()
+api = SafoneAPI()
+userbot = Userbot()
+
+
+from .platforms import *
+
+Apple = AppleAPI()
+Carbon = CarbonAPI()
+SoundCloud = SoundAPI()
+Spotify = SpotifyAPI()
+Resso = RessoAPI()
+Telegram = TeleAPI()
+YouTube = YouTubeAPI()
