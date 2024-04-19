11770453b59ec58e77bb5709e2297048376cd2ae
ZXCVBNMASDFGHJKLQWERTYUIOP0786 <165553468+ZXCVBNMASDFGHJKLQWERTYUIOP0786@users.noreply.github.com>
2024-04-19 15:41:09 +0530

    Update mustjoin.py

--- LUCKYMUSIC/plugins/bot/mustjoin.py
+++ LUCKYMUSIC/plugins/bot/mustjoin.py
@@ -6,7 +6,7 @@ from LUCKYMUSIC import app
 
 #--------------------------
 
-MUST_JOIN = "JARVIS_X_SUPPORT"
+MUST_JOIN = "LuckyXSupport"
 #------------------------
 @app.on_message(filters.incoming & filters.private, group=-1)
 async def must_join_channel(app: Client, msg: Message):
