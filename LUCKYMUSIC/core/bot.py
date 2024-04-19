1216d85d3220d2dfc2acb050ff242443fe9f5148
ZXCVBNMASDFGHJKLQWERTYUIOP0786 <165553468+ZXCVBNMASDFGHJKLQWERTYUIOP0786@users.noreply.github.com>
2024-04-19 15:07:57 +0530

    Update bot.py

--- LUCKYMUSIC/core/bot.py
+++ LUCKYMUSIC/core/bot.py
@@ -10,7 +10,7 @@ class RAJA(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Lucky is on the way...")
         super().__init__(
-            name="𝐀𝐍𝐍𝐈𝐄 𝐗 𝐌𝐔𝐒𝐈𝐂",
+            name="𝐋𝚞𝚌𝚔𝚢🅧𝐌𝚞𝚜𝚒𝚌",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
