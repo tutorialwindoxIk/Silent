d7587f3ff5edda8723ba5d4e5c0bf925dacb6ad7
ZXCVBNMASDFGHJKLQWERTYUIOP0786 <165553468+ZXCVBNMASDFGHJKLQWERTYUIOP0786@users.noreply.github.com>
2024-04-21 14:08:37 +0530

    Update mustjoin.py

--- LUCKYMUSIC/plugins/bot/mustjoin.py
+++ LUCKYMUSIC/plugins/bot/mustjoin.py
@@ -6,6 +6,21 @@ from LUCKYMUSIC import app
 
 #--------------------------
 
+LUCKY_VID = [
+    "https://telegra.ph/file/9b7e1b820c72a14d90be7.mp4",
+    "https://telegra.ph/file/a4d90b0cb759b67d68644.mp4",
+    "https://telegra.ph/file/72f349b1386d6d9374a38.mp4",
+    "https://telegra.ph/file/2b75449612172a96d4599.mp4",
+    "https://telegra.ph/file/b3ac2d77205d5ded860de.mp4",
+    "https://telegra.ph/file/58ae4ac86ef70dc8c8f6a.mp4",
+    "https://telegra.ph/file/c6c1ac9aee4192a8a3747.mp4",
+    "https://telegra.ph/file/55c840c8eba0555318f0d.mp4",
+    "https://telegra.ph/file/e97715885d0a0cfbddaaa.mp4",
+    "https://telegra.ph/file/943bb99829ec526c3f99a.mp4"
+]
+
+#--------------------------
+
 MUST_JOIN = "LuckyXSupport"
 #------------------------
 @app.on_message(filters.incoming & filters.private, group=-1)
