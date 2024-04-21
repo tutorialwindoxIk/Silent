1efc4676ce20af04fc692339676c802e75eed63b
ZXCVBNMASDFGHJKLQWERTYUIOP0786 <165553468+ZXCVBNMASDFGHJKLQWERTYUIOP0786@users.noreply.github.com>
2024-04-21 16:21:34 +0530

    Update repo.py

--- LUCKYMUSIC/plugins/bot/repo.py
+++ LUCKYMUSIC/plugins/bot/repo.py
@@ -1,21 +1,7 @@
-import random
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from LUCKYMUSIC import app
 from config import BOT_USERNAME
-
-LUCKY_VID = [
-    "https://telegra.ph/file/9b7e1b820c72a14d90be7.mp4",
-    "https://telegra.ph/file/a4d90b0cb759b67d68644.mp4",
-    "https://telegra.ph/file/72f349b1386d6d9374a38.mp4",
-    "https://telegra.ph/file/2b75449612172a96d4599.mp4",
-    "https://telegra.ph/file/b3ac2d77205d5ded860de.mp4",
-    "https://telegra.ph/file/58ae4ac86ef70dc8c8f6a.mp4",
-    "https://telegra.ph/file/c6c1ac9aee4192a8a3747.mp4",
-    "https://telegra.ph/file/55c840c8eba0555318f0d.mp4",
-    "https://telegra.ph/file/e97715885d0a0cfbddaaa.mp4",
-    "https://telegra.ph/file/943bb99829ec526c3f99a.mp4"
-]
 
 start_txt = """**
 ✪ ωεℓ¢σмє ƒσя jคяv¡ร яєρσѕ ✪
@@ -38,29 +24,28 @@ start_txt = """**
 
 @app.on_message(filters.command("repo"))
 async def start(_, msg):
-
-   buttons = [
+    buttons = [
      
-            [
+            [ 
             InlineKeyboardButton("ᴀᴅᴅ ᴍᴇ ʙᴀʙᴇs✪", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")
             ],
      
             [
-             InlineKeyboardButton("ᴏᴡɴᴇʀ", url="https://t.me/JARVIS_V2"),
-             InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url="https://t.me/JARVIS_X_SUPPORT"),
+             InlineKeyboardButton("ᴏᴡɴᴇʀ", url="https://t.me/LuckyRaja0"),
+             InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url="https://t.me/LUCKYXSUPPORT"),
              ],
      
              [
              InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ", url="https://t.me/CHATTING_2024"),          
-             InlineKeyboardButton("︎ᴍᴜsɪᴄ", url=f"https://github.com/doraemon890/ANNIE-X-MUSIC"),
+             InlineKeyboardButton("︎ᴍᴜsɪᴄ", url=f"https://github.com/doraemon890/Lucky-X-MUSIC"),
              ],
      
               ]
  
     reply_markup = InlineKeyboardMarkup(buttons)
     
-    await msg.reply_video(
-          random.choice(LUCKY_VID),
+    await msg.reply_photo(
+        photo="https://telegra.ph/file/58afe55fee5ae99d6901b.jpg",
         caption=start_txt,
         reply_markup=reply_markup
     )
