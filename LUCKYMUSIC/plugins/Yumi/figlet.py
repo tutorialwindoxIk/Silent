8bbda4f8024dd8e360027d27eb74ce57c748826b
ZXCVBNMASDFGHJKLQWERTYUIOP0786 <165553468+ZXCVBNMASDFGHJKLQWERTYUIOP0786@users.noreply.github.com>
2024-04-23 14:29:15 +0530

    Update figlet.py

--- LUCKYMUSIC/plugins/Yumi/figlet.py
+++ LUCKYMUSIC/plugins/Yumi/figlet.py
@@ -32,4 +32,4 @@ async def figlet_handler(Client, query: CallbackQuery):
 __mod_name__ = "Fɪɢʟᴇᴛ" 
 __help__="""
 ❍ /figlet*:* ᴍᴀᴋᴇs ғɪɢʟᴇᴛ ᴏғ ᴛʜᴇ ɢɪᴠᴇɴ ᴛᴇxᴛ
-Example:\n\n`/figlet ANNIE `"""
+Example:\n\n`/figlet LUCKY `"""
